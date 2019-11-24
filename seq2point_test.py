cfc3ca204ef92337522c12f5a3dab2ef812cacf8
Jack <jack151298@hotmail.co.uk>
2019-11-24 22:02:07 +0000

    Calculate steps_per_epoch, adds comments, adds early stopping

--- seq2point_test.py
+++ seq2point_test.py
@@ -6,6 +6,7 @@ from model_structure import create_model, load_model
 from data_feeder import TestingChunkSlider
 import matplotlib.pyplot as plt
 
+# Split the test dataset into features and targets.
 def load_dataset(file_name, crop):
     data_frame = pd.read_csv(file_name, nrows=crop, header=0)
     test_input = np.round(np.array(data_frame.iloc[:, 0], float), 6)
@@ -13,6 +14,7 @@ def load_dataset(file_name, crop):
     del data_frame
     return test_input, test_target
 
+# Identify the test file.
 for file_name in os.listdir("./kettle/"):
     if ("TEST" in file_name):
         test_file_name = file_name
@@ -20,28 +22,29 @@ for file_name in os.listdir("./kettle/"):
         print("No testing file was found.")
         break
 
-TEST_FILE_NAME = "./kettle/kettle_test_.csv"
-
 offset = int(0.5 * 599 - 1)
 
-crop = 10000
-test_input, test_target = load_dataset(TEST_FILE_NAME, crop)
+# Get the (cropped) testing dataset.
+CROP = None
+test_input, test_target = load_dataset(test_file_name, CROP)
 
+# Initialise the model and testing generator.
 model = create_model()
-
 test_generator = TestingChunkSlider(number_of_windows=100, offset=offset)
 
+# Test the model.
 testing_history = model.predict_generator(test_generator.load_data(test_input), steps=1)
 
+# Can't have negative energy readings - set any results below 0 to 0.
 test_target[test_target < 0] = 0
 testing_history[testing_history < 0] = 0
 
+# Plot testing outcomes against ground truth.
 plt.plot(testing_history, label="Testing")
-plt.plot(test_target[0 : testing_history.size], label="Truth")
+plt.plot(test_target[0 : testing_history.size], label="Ground Truth")
 plt.title('Testing Results')
 plt.ylabel('Prediction')
 plt.xlabel('Testing Iteration')
-plt.ylim(-100, 100)
 plt.legend()
 plt.savefig(fname="testing_results.png")
 plt.show()
--- seq2point_train.py
+++ seq2point_train.py
@@ -9,22 +9,36 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Static maximum chunk size and batch size.
 SIZE_OF_CHUNK = 5 * 10 ** 2
+DEFAULT_BATCH_SIZE = 1000
 
 training_directory = "./kettle/kettle_training_.csv"
 validation_directory = "./kettle/kettle_validation_.csv"
 window_offset = int((0.5 * 599 ) - 1)
 
-training_chunker = InputChunkSlider(file_name=training_directory, chunk_size=SIZE_OF_CHUNK, batch_size=1000, crop=10000,shuffle=True, offset=window_offset, header=0, ram_threshold=5*10**5)
+# Generator function to produce batches.
+training_chunker = InputChunkSlider(file_name=training_directory, chunk_size=SIZE_OF_CHUNK, batch_size=DEFAULT_BATCH_SIZE, crop=None, shuffle=True, offset=window_offset, header=0, ram_threshold=5*10**5)
+
+# Calculate the optimum steps per epoch.
+training_chunker.check_if_chunking()
+steps_per_epoch = np.round(int(training_chunker.total_size / DEFAULT_BATCH_SIZE), decimals=0)
+
+# Compile the model with an Adam optimiser. Initialise early stopping callback that stops only 
+# if there's no improvement 2 epochs later.
 model = create_model()
-model.compile(Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999), loss="mean_squared_error", metrics=["mae"])
-training_history = model.fit_generator(training_chunker.load_dataset(), steps_per_epoch=1, epochs=4, verbose=2, callbacks=None)
+model.compile(Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999), loss="mean_squared_error", metrics=["mae", "mse"]) 
+early_stopping = EarlyStopping(monitor="loss", min_delta=0, patience=2, verbose=2, mode="auto")
 
+# Train the model with the generator function and early stopping callback.
+training_history = model.fit_generator(training_chunker.load_dataset(), steps_per_epoch=steps_per_epoch, epochs=10, verbose=2, callbacks=[early_stopping])
 
-
-plt.plot(training_history.history['loss'], label="Training")
-plt.title('Model Loss')
-plt.ylabel('Loss')
+# Plot the monitored metrics and save.
+plt.plot(training_history.history['loss'], label="Loss")
+plt.plot(training_history.history['mae'], label="MAE")
+plt.plot(training_history.history['mse'], label="MSE")
+plt.title('Training Metrics')
+plt.ylabel('y')
 plt.xlabel('Epoch')
 plt.legend()
 plt.savefig(fname="training_results.png")
