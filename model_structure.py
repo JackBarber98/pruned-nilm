41e5004826aee664fb7d0cdcd658d1db13646595
JackBarber98 <jack151298@hotmail.co.uk>
2020-01-16 14:17:18 +0000

    Adds TFLite conversion (no interpretation yet)

--- model_structure.py
+++ model_structure.py
@@ -13,7 +13,7 @@ def create_model():
 
     input_layer = tf.keras.layers.Input(shape=(599,))
     reshape_layer = tf.keras.layers.Reshape((1, 599, 1))(input_layer)
-    conv_layer_1 = tf.keras.layers.Convolution2D(filters=30, kernel_size=(10, 1), strides=(1, 1), padding="same", activation="relu")(reshape_layer)
+    conv_layer_1 = tf.keras.layers.Convolution2D(filters=30, kernel_size=(10, 1), strides=(1, 1), padding="same", activation="relu")(reshape_layer)zz
     conv_layer_2 = tf.keras.layers.Convolution2D(filters=30, kernel_size=(8, 1), strides=(1, 1), padding="same", activation="relu")(conv_layer_1)
     conv_layer_3 = tf.keras.layers.Convolution2D(filters=40, kernel_size=(6, 1), strides=(1, 1), padding="same", activation="relu")(conv_layer_2)
     conv_layer_4 = tf.keras.layers.Convolution2D(filters=50, kernel_size=(5, 1), strides=(1, 1), padding="same", activation="relu")(conv_layer_3)
@@ -24,6 +24,43 @@ def create_model():
 
     model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
     return model
+
+def create_dropout__model():
+
+    """Specifies the structure of a seq2point model using Keras' functional API.
+
+    Returns:
+    model (tensorflow.keras.Model): The uncompiled seq2point model with dropout layers.
+
+    """
+
+    input_layer = tf.keras.layers.Input(shape=(599,))
+    reshape_layer = tf.keras.layers.Reshape((1, 599, 1))(input_layer)
+    conv_layer_1 = tf.keras.layers.Convolution2D(filters=30, kernel_size=(10, 1), strides=(1, 1), padding="same", activation="relu")(reshape_layer)
+    dropout_layer_1 = tf.keras.layers.Dropout(0.5)(conv_layer_1)
+    conv_layer_2 = tf.keras.layers.Convolution2D(filters=30, kernel_size=(8, 1), strides=(1, 1), padding="same", activation="relu")(dropout_layer_1)
+    dropout_layer_2 = tf.keras.layers.Dropout(0.5)(conv_layer_2)
+    conv_layer_3 = tf.keras.layers.Convolution2D(filters=40, kernel_size=(6, 1), strides=(1, 1), padding="same", activation="relu")(dropout_layer_2)
+    dropout_layer_3 = tf.keras.layers.Dropout(0.5)(conv_layer_3)
+    conv_layer_4 = tf.keras.layers.Convolution2D(filters=50, kernel_size=(5, 1), strides=(1, 1), padding="same", activation="relu")(dropout_layer_3)
+    dropout_layer_4 = tf.keras.layers.Dropout(0.5)(conv_layer_4)
+    conv_layer_5 = tf.keras.layers.Convolution2D(filters=50, kernel_size=(5, 1), strides=(1, 1), padding="same", activation="relu")(dropout_layer_4)
+    dropout_layer_5 = tf.keras.layers.Dropout(0.5)(conv_layer_5)
+    flatten_layer = tf.keras.layers.Flatten()(dropout_layer_5)
+    label_layer = tf.keras.layers.Dense(1024, activation="relu")(flatten_layer)
+    dropout_layer_6 = tf.keras.layers.Dropout(0.5)(label_layer)
+    output_layer = tf.keras.layers.Dense(1, activation="linear")(dropout_layer_6)
+
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+    return model
+
+def create_lite_model(path):
+    converter = tf.lite.TFLiteConverter.from_saved_model(path + "_weights.h5")
+    lite_model = converter.convert()
+    
+    if not os.path.exists(path + "_lite.tflite"):
+        open((path + "_lite.tflite"), 'a').close()
+    lite_model.save(path + "_lite.tflite")
 
 def save_model(model, path):
 
